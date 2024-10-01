eed4166836818b9aff1a039067ba7d45f8a93fa8
jorge-vscode <jorge.vscode@gmail.com>
2024-10-01 23:26:51 +0200

    Learncpp forward declaration, file linking and function use

---
+++ Unit2/DoubleNumber/DoubleNumber.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int userInput()
+{
+
+    int input{};
+
+    std::cout << "Input an integer: ";
+    std::cin  >> input;
+
+    return input;
+
+}
+
+int doubleInteger(int x)
+{
+
+    return 2 * x;
+
+}
+
+int main()
+{
+
+    int number{userInput()};
+    std::cout << "This number's double is: " << doubleInteger(number);
+
+    return 0;
+    
+}
