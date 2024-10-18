0ab0435a3d26059b2537bc6a63e75cbbc6065500
jorge-vscode <jorge.vscode@gmail.com>
2024-10-19 00:33:34 +0200

    has errors began with find possible moves

--- PersonalProjects/Chess/main.cpp
+++ PersonalProjects/Chess/main.cpp
@@ -1,11 +1,19 @@
 #include "board.h"
 #include "pieces.h"
 
-int main(){
-    Board board;
+int main()
+{
+ 
     setupPieces();
-    board.updateCurrentBoardStatus();
-    board.showBoard();
+    for(auto& obj : pieces){
+        if(obj.getPositionX()!=1 && obj.getPositionY()!=1){
+            continue;
+        }
+        obj.findPossibleMoves();
+        auto aux = obj.getPossibleMoves();
+        for(auto& move : aux){
+            std::cout<<"("<<move[0]<<","<<move[1]<<")"<<'\n';
+        }
+    }
     return 0;
 }
-
